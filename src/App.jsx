1b7b074eccc3a756350d3e77ac55ff2936be9db1
Bdmnss <lukabadali123@gmail.com>
2024-06-14 18:23:30 +0400

    adding body and section styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ function App() {
   const [notifications, setNotifications] = useState(data);
 
   return (
-    <>
+    <div className="app-div">
       <section>
-        <div>
-          <div>
+        <div className="section-div">
+          <div className="notifications-div">
             <h2>Notifications</h2>
             <div>3</div>
           </div>
           <p>Mark all as read</p>
         </div>
       </section>
-    </>
+    </div>
   );
 }
 
