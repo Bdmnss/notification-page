ec9a33cf0af817f81912f813d31d521345b1b812
Bdmnss <lukabadali123@gmail.com>
2024-06-14 18:04:16 +0400

    importing data.json and adding it to useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import "./App.css";
+import data from "./data.json";
 
 function App() {
+  const [notifications, setNotifications] = useState(data);
+
   return <></>;
 }
 
