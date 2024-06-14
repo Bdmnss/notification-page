45fe8efec0855032056e77ea20bb5d416da6515b
Bdmnss <lukabadali123@gmail.com>
2024-06-14 18:08:46 +0400

    adding section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,19 @@ import data from "./data.json";
 function App() {
   const [notifications, setNotifications] = useState(data);
 
-  return <></>;
+  return (
+    <>
+      <section>
+        <div>
+          <div>
+            <h2>Notifications</h2>
+            <div>3</div>
+          </div>
+          <p>Mark all as read</p>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
